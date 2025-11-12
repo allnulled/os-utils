91949a626a13c3efb14ed7630f127fe1fcaa13ef
carlosjimenohernandez <jimenohernandezcarlos@gmail.com>
2025-11-12 18:53:49 +0100

    package.json

--- test/test.js
+++ test/test.js
@@ -1,10 +1,8 @@
-const fs = require("fs");
 const path = require("path");
 const frameworkPath = path.resolve(__dirname, "..", "os-utils.js")
 const OsUtils = require(frameworkPath);
 
-OsUtils
-  .changeDirectory(__dirname)
-  .select("**/*.txt")
-  .execute(file => `cat ${JSON.stringify(file)}`);
+const fileSelection = OsUtils.changeDirectory(__dirname).select("**/*.txt");
 
+fileSelection.execute("`cat ${file}`");
+fileSelection.execute(file => `cat ${file}`);
